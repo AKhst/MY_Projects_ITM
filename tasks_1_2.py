8763926fe2d6c9d138eaed588ced09622d38830f
AKhst <stroitop214@gmail.com>
2023-11-19 21:30:29 +0100

    The last version

--- tasks_1_2.py
+++ tasks_1_2.py
@@ -2,19 +2,15 @@ import logging
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, filename="example.log", filemode="w")
 
-
 def divide(a: int, b: int):
     try:
         # Logging input values
         logger.info(f'На вход поданы значения переменных a:{a}, b:{b}')
-
         # Checking for division by zero
         if b == 0:
             raise ZeroDivisionError("Попытка деления на ноль")
-
         # Performing division
         result = a / b
-
         # Logging the result
         logger.info(f'Привет, ваш результат деления: {result}')
         return result
@@ -25,15 +21,12 @@ def divide(a: int, b: int):
         # Logging information about the finally block
         logger.info("Этот блок выполняется всегда")
 
-
 if __name__ == '__main__':
     try:
         # Getting input values from the user
         a, b = int(input("Введите значение a: ")), int(input("Введите значение b: "))
-
         # Calling the divide function
         result = divide(a, b)
-
         # Displaying the result
         print(f'Результат деления: {result}')
     except Exception as er:
