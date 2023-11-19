581ec8d682ebac002eaf88575bb2d38886e62956
AKhst <stroitop214@gmail.com>
2023-11-19 18:22:22 +0100

    The last version

--- tasks_1_2.py
+++ tasks_1_2.py
@@ -1,5 +1,44 @@
 import logging
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, filename="example.log", filemode="w")
 
-# Проверьте атрибуты объекта logging
-if hasattr(logging, 'Logger') and hasattr(logging, 'DEBUG'):
-    print("Logging module has been successfully imported.")
+
+def divide(a: int, b: int):
+    try:
+        # Logging input values
+        logger.info(f'На вход поданы значения переменных a:{a}, b:{b}')
+
+        # Checking for division by zero
+        if b == 0:
+            raise ZeroDivisionError("Попытка деления на ноль")
+
+        # Performing division
+        result = a / b
+
+        # Logging the result
+        logger.info(f'Привет, ваш результат деления: {result}')
+        return result
+    except (ValueError, ZeroDivisionError, Exception) as e:
+        # Logging errors
+        logger.error(f"Ошибка в модуле функции: {e}", exc_info=True)
+    finally:
+        # Logging information about the finally block
+        logger.info("Этот блок выполняется всегда")
+
+
+if __name__ == '__main__':
+    try:
+        # Getting input values from the user
+        a, b = int(input("Введите значение a: ")), int(input("Введите значение b: "))
+
+        # Calling the divide function
+        result = divide(a, b)
+
+        # Displaying the result
+        print(f'Результат деления: {result}')
+    except Exception as er:
+        # Logging errors from the main block
+        logger.error(f"Ошибка из основного блока программы: {er}", exc_info=True)
+    finally:
+        # Logging information about the finally block in the main block
+        logger.info("Этот блок выполняется всегда")
